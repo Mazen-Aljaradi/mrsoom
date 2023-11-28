82bab30db38afd9ef9c97ab6298b7c1075a4a621
Mazen-Aljaradi <31169139+Mazen-Aljaradi@users.noreply.github.com>
2023-11-29 00:43:41 +0300

    first commit

---
+++ utils/data/products-colors.ts
@@ -0,0 +1,34 @@
+export const productsColors = [
+  {
+    id: '1',
+    label: 'احمر',
+    color: '#d10230',
+  },
+  // {
+  //   id: '2',
+  //   label: 'بني',
+  //   color: '#6F3E18',
+  // },
+  // {
+  //   id: '3',
+  //   label: 'أصفر',
+  //   color: '#D4BE8D',
+  // },
+  // {
+  //   id: '4',
+  //   label: 'رمادي',
+  //   color: '#838383',
+  // },
+  // {
+  //   id: '5',
+  //   label: 'أبيض',
+  //   color: '#F3F3F3',
+  // },
+  // {
+  //   id: '6',
+  //   label: 'أزرق',
+  //   color: '#0F73AD',
+  // },
+];
+
+export default productsColors;
---
+++ utils/data/products-sizes.ts
@@ -0,0 +1,34 @@
+export const productsSizes = [
+    {
+      id: '1',
+      // label: 'XS',
+      label: '52',
+    },
+    {
+      id: '2',
+      // label: 'S',
+      label: '55',
+    },
+    {
+      id: '3',
+      // label: 'M',
+      label: '58',
+    },
+    {
+      id: '4',
+      // label: 'L',
+      label: '60',
+    },
+    {
+      id: '5',
+      // label: 'XL',
+      label: '62',
+    },
+    // {
+    //   id: '6',
+    //   // label: 'XXL',
+    //   label: 'XXL',
+    // },
+  ];
+  
+  export default productsSizes;
