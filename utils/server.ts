6892402eebc02f2b3cb8f0e89986321a7e3281fd
Mazen-Aljaradi <31169139+Mazen-Aljaradi@users.noreply.github.com>
2023-11-29 01:51:12 +0300

    Update server.ts

--- utils/server.ts
+++ utils/server.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'mrsoom-72g7w2rn1-mazen-aljaradi.vercel.app';
+export const server = dev ? 'mrsoom.vercel.app' : 'mrsoom.vercel.app';
